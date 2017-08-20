6971f80e346773f4cb524c22b10f022f7ba45f17
Chauncey Wang <chncwang@gmail.com>
2017-08-20 20:23:27 +0800

    Update latest official N3LDG

--- Metric.h
+++ Metric.h
@@ -13,78 +13,59 @@ using namespace std;
 
 struct Metric {
 
-public:
-  int overall_label_count;
-  int correct_label_count;
-  int predicated_label_count;
+  public:
+    int overall_label_count;
+    int correct_label_count;
+    int predicated_label_count;
 
-public:
-  Metric()
-  {
-    overall_label_count = 0;
-    correct_label_count = 0;
-    predicated_label_count = 0;
-  }
-
-  ~Metric() {}
-
-  void reset()
-  {
-    overall_label_count = 0;
-    correct_label_count = 0;
-    predicated_label_count = 0;
-  }
-
-  bool bIdentical()
-  {
-    if (predicated_label_count == 0)
-    {
-      if (overall_label_count == correct_label_count)
-      {
-        return true;
-      }
-      return false;
+  public:
+    Metric() {
+        overall_label_count = 0;
+        correct_label_count = 0;
+        predicated_label_count = 0;
     }
-    else
-    {
-      if (overall_label_count == correct_label_count && predicated_label_count == correct_label_count)
-      {
-        return true;
-      }
-      return false;
-    }
-  }
 
-  double getAccuracy()
-  {
-    if (predicated_label_count == 0)
-    {
-      return correct_label_count*1.0 / overall_label_count;
-    }
-    else
-    {
-      return correct_label_count*2.0 / (overall_label_count + predicated_label_count);
-    }
-  }
+    ~Metric() {}
 
-  double getFMeasure() {
-	  return correct_label_count*2.0 / (overall_label_count + predicated_label_count);
-  }
+    void reset() {
+        overall_label_count = 0;
+        correct_label_count = 0;
+        predicated_label_count = 0;
+    }
 
-  void print()
-  {
-    if (predicated_label_count == 0)
-    {
-      std::cout << "Accuracy:\tP=" << correct_label_count << "/" << overall_label_count
-        << "=" << correct_label_count*1.0 / overall_label_count << std::endl;
+    bool bIdentical() {
+        if (predicated_label_count == 0) {
+            if (overall_label_count == correct_label_count) {
+                return true;
+            }
+            return false;
+        } else {
+            if (overall_label_count == correct_label_count && predicated_label_count == correct_label_count) {
+                return true;
+            }
+            return false;
+        }
     }
-    else
-    {
-      std::cout << "Recall:\tP=" << correct_label_count << "/" << overall_label_count << "=" << correct_label_count*1.0 / overall_label_count
-        << ", " << "Accuracy:\tP=" << correct_label_count << "/" << predicated_label_count << "=" << correct_label_count*1.0 / predicated_label_count
-        << ", " << "Fmeasure:\t" << correct_label_count*2.0 / (overall_label_count + predicated_label_count) << std::endl;
+
+    double getAccuracy() {
+        if (predicated_label_count == 0) {
+            return correct_label_count*1.0 / overall_label_count;
+        } else {
+            return correct_label_count*2.0 / (overall_label_count + predicated_label_count);
+        }
     }
-  }
+
+
+    void print() {
+        if (predicated_label_count == 0) {
+            std::cout << "Accuracy:\tP=" << correct_label_count << "/" << overall_label_count
+                      << "=" << correct_label_count*1.0 / overall_label_count << std::endl;
+        } else {
+            std::cout << "Recall:\tP=" << correct_label_count << "/" << overall_label_count << "=" << correct_label_count*1.0 / overall_label_count
+                      << ", " << "Accuracy:\tP=" << correct_label_count << "/" << predicated_label_count << "=" << correct_label_count*1.0 / predicated_label_count
+                      << ", " << "Fmeasure:\t" << correct_label_count*2.0 / (overall_label_count + predicated_label_count) << std::endl;
+        }
+    }
 
 
 };
--- Node.h
+++ Node.h
@@ -21,109 +21,106 @@ class Execute;
 // one Node means a vector
 // the col should be 1, because we aimed for NLP only
 class Node {
-public:
-  vector<Node*> parents;
-public:
-  Tensor1D val;
-  Tensor1D loss;
-public:
-  int dim;
-  int degree;
-  string node_type;
-  string tag;
+  public:
+    vector<Node*> parents;
+  public:
+    Tensor1D val;
+    Tensor1D loss;
+  public:
+    int dim;
+    int degree;
+    string node_type;
 
-public:
-  Tensor1D drop_mask;
-  dtype drop_value;
-
-
-public:
-  Node() {
-    dim = 0;
-    degree = 0;
-    parents.clear();
-    node_type = "interface";
-    drop_value = -1;
-  }
-
-  virtual ~Node() {
-    val = 0;
-    loss = 0;
-    degree = 0;
-    parents.clear();
-    node_type.clear();
-  }
+  public:
+    Tensor1D drop_mask;
+    dtype drop_value;
 
 
-public:
-  virtual inline void clearValue() {
-    val = 0;
-    loss = 0;
-    degree = 0;
-    if (drop_value > 0)drop_mask = 1;
-    parents.clear();
-  }
-
-  virtual inline void init(int ndim, dtype dropout, AlignedMemoryPool* mem = NULL) {
-    dim = ndim;
-    val.init(dim, mem);
-    loss.init(dim, mem);
-    drop_mask.init(dim, mem);
-    if (dropout > 0 && dropout <= 1) {
-      drop_value = dropout;
+  public:
+    Node() {
+        dim = 0;
+        degree = 0;
+        parents.clear();
+        node_type = "interface";
+        drop_value = -1;
     }
-    else {
-      drop_value = -1;
+
+    virtual ~Node() {
+        val = 0;
+        loss = 0;
+        degree = 0;
+        parents.clear();
+        node_type.clear();
     }
-    parents.clear();
-  }
 
 
-  inline void forward_drop(bool bTrain) {
-    if (drop_value > 0) {
-      if (bTrain) {
-        int dropNum = (int)(dim * drop_value);
-        vector<int> tmp_masks(dim);
-        for (int idx = 0; idx < dim; idx++) {
-          tmp_masks[idx] = idx < dropNum ? 0 : 1;
+  public:
+    virtual inline void clearValue() {
+        val = 0;
+        loss = 0;
+        degree = 0;
+        if (drop_value > 0)drop_mask = 1;
+        parents.clear();
+    }
+
+    virtual inline void init(int ndim, dtype dropout) {
+        dim = ndim;
+        val.init(dim);
+        loss.init(dim);
+        drop_mask.init(dim);
+        if (dropout > 0 && dropout <= 1) {
+            drop_value = dropout;
+        } else {
+            drop_value = -1;
         }
-        random_shuffle(tmp_masks.begin(), tmp_masks.end());
-        for (int idx = 0; idx < dim; idx++) {
-          drop_mask[idx] = tmp_masks[idx];
+        parents.clear();
+    }
+
+
+    inline void forward_drop(bool bTrain) {
+        if (drop_value > 0) {
+            if (bTrain) {
+                int dropNum = (int)(dim * drop_value);
+                vector<int> tmp_masks(dim);
+                for (int idx = 0; idx < dim; idx++) {
+                    tmp_masks[idx] = idx < dropNum ? 0 : 1;
+                }
+                random_shuffle(tmp_masks.begin(), tmp_masks.end());
+                for (int idx = 0; idx < dim; idx++) {
+                    drop_mask[idx] = tmp_masks[idx];
+                }
+                val.vec() = val.vec() * drop_mask.vec();
+            } else {
+                val.vec() = val.vec() * (1 - drop_value);
+            }
         }
-        val.vec() = val.vec() * drop_mask.vec();
-      }
-      else {
-        val.vec() = val.vec() * (1 - drop_value);
-      }
+        degree = -1;
     }
-    degree = -1;
-  }
 
-  inline void backward_drop() {
-    if (drop_value > 0) {
-      loss.vec() = loss.vec() * drop_mask.vec();
+    inline void backward_drop() {
+        if (drop_value > 0) {
+            loss.vec() = loss.vec() * drop_mask.vec();
+        }
     }
-  }
 
-public:
+  public:
 
-  virtual inline Execute* generate(bool bTrain) = 0;
+    virtual inline Execute* generate(bool bTrain) = 0;
 
-  virtual inline bool typeEqual(Node* other) {
-    if (node_type.compare(other->node_type) == 0) {
-      return true;
+    virtual inline bool typeEqual(Node* other) {
+        if (node_type.compare(other->node_type) == 0) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-public:
-  virtual inline void addParent(Node* parent) {
-    if (degree >= 0) {
-      parents.push_back(parent);
-      parent->degree++;
+  public:
+    virtual inline void addParent(Node* parent) {
+        if (degree >= 0) {
+            parents.push_back(parent);
+            parent->degree++;
+        }
     }
-  }
 
 
 };
@@ -132,32 +129,32 @@ typedef  Node* PNode;
 
 
 class Execute {
-public:
-  vector<PNode> batch;
+  public:
+    vector<PNode> batch;
 
-public:
-  virtual ~Execute() {
-    batch.clear();
-  }
-
-public:
-  virtual inline void forward() = 0;
-  virtual inline void backward() = 0;
-
-
-  virtual inline bool addNode(PNode in) {
-    if (batch.empty()) {
-      std::cout << "empty batch, strange...." << std::endl;
-      return false;
+  public:
+    virtual ~Execute() {
+        batch.clear();
     }
 
-    if (batch[0]->typeEqual(in)) {
-      batch.push_back(in);
-      return true;
-    }
+  public:
+    virtual inline void forward() = 0;
+    virtual inline void backward() = 0;
 
-    return false;
-  }
+
+    virtual inline bool addNode(PNode in) {
+        if (batch.empty()) {
+            std::cout << "empty batch, strange...." << std::endl;
+            return false;
+        }
+
+        if (batch[0]->typeEqual(in)) {
+            batch.push_back(in);
+            return true;
+        }
+
+        return false;
+    }
 };
 
 
